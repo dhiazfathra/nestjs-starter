2e7b99b5af7bf1e94864d4d555ec8d17353b1e94
dhiazfathra <dhiazfathra@gmail.com>
2025-05-17 21:24:33 +0700

    test: add Jest JUnit test reporting & Codecov test analytics integration (#11)
    * test: add Jest JUnit test reporting & Codecov test analytics integration
    
    * feat: add Codecov test analytics with JUnit XML reporting

---
+++ jest-junit.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  classNameTemplate: "{filepath}",
+  ancestorSeparator: " › ",
+  titleTemplate: "{title}",
+  outputDirectory: "./junit",
+  outputName: "junit.xml",
+  suiteNameTemplate: "{filepath}",
+  uniqueOutputName: false,
+  addFileAttribute: true
+};
