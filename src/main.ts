69a069460f05fd0ee8f5a4dd55cf7d0e1b9db973
dhiazfathra <dhiazfathra@gmail.com>
2025-05-17 19:21:47 +0700

    ci: add semantic release workflow with changelog generation (#2)
    * feat: init project with auth & user management
    
    * ci: add semantic release workflow with changelog generation

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 /**
  * Initializes and starts the NestJS application with global configuration.
